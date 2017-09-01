db1c1331d4eddb484dc63b8271abe416aff45f40
Woyten <woyten.tielesch@online.de>
2017-09-01 22:24:44 +0200

    Styling

--- client/Main.elm
+++ client/Main.elm
@@ -1,12 +1,13 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Html.Attributes as Attributes
+import Html.Events as Events
 import Http
-import Response
 import Post
 import Request
+import Response
+import Style
 
 
 main =
@@ -41,7 +42,7 @@ type alias Model =
 
 
 init =
-    ( { status = Initial, user = "" }, Cmd.none )
+    { status = Initial, user = "" } ! []
 
 
 
@@ -83,64 +84,65 @@ handleUserInput userInput model =
 
 
 view model =
-    div [] (createDynamicControls model)
+    div [ Style.mainDiv ] [ div [ Style.loginDiv ] (createDynamicControls model) ]
 
 
 createDynamicControls model =
     case model.status of
         Initial ->
-            defaultControls model
+            defaultControls model ++ [ multiline "" " " ]
 
         HttpSuccess Response.Accepted ->
-            defaultControls model ++ label "green" "Request accepted"
+            defaultControls model
+                ++ [ multiline "green" "Request accepted" ]
 
         HttpSuccess Response.AlreadyRegistered ->
-            defaultControls model ++ label "blue" "Coder already registered"
+            defaultControls model
+                ++ [ multiline "blue" "Coder already registered" ]
 
         HttpSuccess Response.NoReviewerNeeded ->
-            defaultControls model ++ label "blue" "No review open"
+            defaultControls model
+                ++ [ multiline "blue" "No review open" ]
 
         HttpSuccess (Response.NeedsReviewer coder review_id) ->
             askForConfirmation coder review_id
 
         HttpSuccess Response.ReviewNotFound ->
-            defaultControls model ++ label "red" "The review could not be accepted.\nYou probably ran into a timeout."
+            defaultControls model
+                ++ [ multiline "red" "Review not found. You probably ran into a timeout." ]
 
         HttpFailure (Http.BadPayload errMsg _) ->
             defaultControls model
-                ++ label "red" "Invalid server payload:"
-                ++ label "black" errMsg
+                ++ [ multiline "red" "Invalid server payload:"
+                   , multiline "black" errMsg
+                   ]
 
         HttpFailure otherError ->
             defaultControls model
-                ++ label "red" "HTTP error:"
-                ++ label "black" (toString otherError)
 
 
 defaultControls model =
-    wrapDivs
-        [ text "Name:"
-        , input [ value model.user, onInput (UserInput << ReviewerInputUpdate) ] []
-        , button [ onClick (UserInput NeedReviewer) ] [ text "I need a review" ]
-        , button [ onClick (UserInput HaveTimeForReview) ] [ text "I have time for a review" ]
+    [ label [ Style.text "black", Attributes.for "text_input" ] [ text "Name:" ]
+    , input [ Style.textField, Attributes.id "text_input", Attributes.value model.user, Events.onInput (UserInput << ReviewerInputUpdate) ] []
+    , div [ Style.buttonBox ]
+        [ button [ Style.button, Events.onClick (UserInput NeedReviewer) ] [ text "I need a reviewer" ]
+        , button [ Style.button, Events.onClick (UserInput HaveTimeForReview) ] [ text "I have time for a review" ]
         ]
+    ]
 
 
 askForConfirmation coder review_id =
-    wrapDivs
-        [ b [] [ text coder ]
-        , text " needs a review first. Will you do the review?"
-        , button [ onClick (UserInput <| WillReview review_id) ] [ text "Yes" ]
-        , button [ onClick (UserInput <| WontReview review_id) ] [ text "No" ]
+    [ div [ Style.text "black" ] [ text "The following person needs a review:" ]
+    , div [ Style.coder ] [ text coder ]
+    , div [ Style.buttonBox ]
+        [ button [ Style.button, Events.onClick (UserInput <| WillReview review_id) ] [ text "I will do it" ]
+        , button [ Style.button, Events.onClick (UserInput <| WontReview review_id) ] [ text "I won't do it" ]
         ]
+    ]
 
 
-wrapDivs =
-    List.map <| \x -> div [] [ x ]
-
-
-label color multilineString =
-    multilineString
+multiline color string =
+    string
         |> String.lines
-        |> List.map text
-        |> List.map (\x -> div [ style [ ( "color", color ) ] ] [ x ])
+        |> List.map (text >> List.singleton >> div [])
+        |> div [ Style.text color ]
