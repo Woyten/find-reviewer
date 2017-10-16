0a6645d3510a65fb7b28509679910f56c0d858c9
Philipp Vollmer <briefe@kebes.de>
2017-10-16 21:37:21 +0200

    Fixed and extended unit tests

--- client/tests/Tests.elm
+++ client/tests/Tests.elm
@@ -6,18 +6,52 @@ import Response
 import Json.Decode
 
 
+requestsWithoutParams : List Response.Response
+requestsWithoutParams =
+    [ Response.UnknownIdentity
+    , Response.Accepted
+    , Response.AlreadyRegistered
+    , Response.NoReviewerNeeded
+    , Response.ReviewNotFound
+    ]
+
+
 suite : Test
 suite =
-    describe "decoder tests"
+    describe "decoder tests without params"
         (List.map
             (\x ->
                 test ("Parses " ++ toString x)
                     (\() ->
                         Expect.equal (Ok x)
-                            (Json.Decode.decodeString Response.responseDecoder
-                                ("\"" ++ toString x ++ "\"")
+                            (Json.Decode.decodeString Response.decode
+                                ("{\"" ++ toString x ++ "\": {}}")
                             )
                     )
             )
-            Response.requestsWithoutParams
+            requestsWithoutParams
         )
+
+
+suite2 : Test
+suite2 =
+    describe "decoder test with params"
+        [ test
+            ("Parses KnownIdentity")
+            (\() ->
+                Expect.equal (Ok (Response.KnownIdentity "Kebes"))
+                    (Json.Decode.decodeString
+                        Response.decode
+                        ("{\"KnownIdentity\": {\"username\":\"Kebes\" }}")
+                    )
+            )
+        , test
+            ("Parses NeedsReviewer")
+            (\() ->
+                Expect.equal (Ok (Response.NeedsReviewer "Kebes" 12345))
+                    (Json.Decode.decodeString
+                        Response.decode
+                        ("{\"NeedsReviewer\": {\"coder\":\"Kebes\", \"review_id\":12345 }}")
+                    )
+            )
+        ]
